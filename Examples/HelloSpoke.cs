7bc3649c49a25767ed44ac973a042d770f87b27f
Adam Alexander <16426184+Adam4lexander@users.noreply.github.com>
2025-05-07 22:47:44 +1000

    first example debug messages

--- Examples/HelloSpoke.cs
+++ Examples/HelloSpoke.cs
@@ -9,25 +9,25 @@ namespace Spoke.Examples {
             // This block runs when the behaviour is Awake
             s.UsePhase(IsAwake, s => {
                 Debug.Log("IsAwake Mounted");
-                s.OnCleanup(() => Debug.Log("IsAwake Disposed (IsDestroyed)"));
+                s.OnCleanup(() => Debug.Log("Disposing IsAwake"));
             });
 
             // This block runs when the behaviour is Enabled
             s.UsePhase(IsEnabled, s => {
                 Debug.Log("IsEnabled Mounted");
-                s.OnCleanup(() => Debug.Log("IsEnabled Disposed (IsDisabled)"));
+                s.OnCleanup(() => Debug.Log("Disposing IsEnabled"));
             });
 
             // This block runs when the behaviour is Started
             s.UsePhase(IsStarted, s => {
                 Debug.Log("IsStarted Mounted");
-                s.OnCleanup(() => Debug.Log("IsStarted Disposed"));
+                s.OnCleanup(() => Debug.Log("Disposing IsStarted"));
             });
 
             // Init is also a declarative block, just like the phases above.
             // It runs before the behaviour is Awake.
             Debug.Log("Init Mounted");
-            s.OnCleanup(() => Debug.Log("Init Disposed"));
+            s.OnCleanup(() => Debug.Log("Disposing Init"));
 
         }
 
