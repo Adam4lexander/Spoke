758f7b9ae20358411257e007cfac80c9c0179fe3
Adam Alexander <16426184+Adam4lexander@users.noreply.github.com>
2025-05-07 21:04:52 +1000

    first example

---
+++ Examples/HelloSpoke.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Spoke.Examples {
+    
+    public class HelloSpoke : SpokeBehaviour {
+     
+        protected override void Init(EffectBuilder s) {
+
+            // This block runs when the behaviour is Awake
+            s.UsePhase(IsAwake, s => {
+                Debug.Log("IsAwake Mounted");
+                s.OnCleanup(() => Debug.Log("IsAwake Disposed (IsDestroyed)"));
+            });
+
+            // This block runs when the behaviour is Enabled
+            s.UsePhase(IsEnabled, s => {
+                Debug.Log("IsEnabled Mounted");
+                s.OnCleanup(() => Debug.Log("IsEnabled Disposed (IsDisabled)"));
+            });
+
+            // This block runs when the behaviour is Started
+            s.UsePhase(IsStarted, s => {
+                Debug.Log("IsStarted Mounted");
+                s.OnCleanup(() => Debug.Log("IsStarted Disposed"));
+            });
+
+            // Init is also a declarative block, just like the phases above.
+            // It runs before the behaviour is Awake.
+            Debug.Log("Init Mounted");
+            s.OnCleanup(() => Debug.Log("Init Disposed"));
+
+        }
+
+    }
+
+}
