2826c9c4dba820a692687c20e63b8e40514b1a3d
Adam Alexander <16426184+Adam4lexander@users.noreply.github.com>
2026-06-26 16:37:57 +1000

    turret

---
+++ Examples/05_BaseDefence/Scripts/Turret.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spoke.Examples.BaseDefence {
+
+    public class Turret : SpokeBehaviour {
+
+        [Header("References")]
+        [SerializeField] GameObject pivot;
+        [SerializeField] GameObject fireFrom;
+
+        [Header("Attributes")]
+        [SerializeField] float range;
+        [SerializeField] float rotationSpeed;
+
+        Vector3 targetDirection = Vector3.zero;
+
+        protected override void Init(EffectBuilder s) {
+            targetDirection = fireFrom.transform.forward;
+
+            s.Phase(IsEnabled, s => {
+                s.Effect(RotateToTarget);
+                s.Effect(IdleBehaviour);
+            });
+        }
+
+        EffectBlock RotateToTarget => s => {
+            IEnumerator onUpdate() {
+                while (true) {
+                    var flat = Vector3.ProjectOnPlane(targetDirection, Vector3.up);
+                    if (flat.sqrMagnitude > 0.0001f) {
+                        var target = Quaternion.LookRotation(flat, Vector3.up);
+                        pivot.transform.rotation = Quaternion.RotateTowards(pivot.transform.rotation, target, rotationSpeed * Time.deltaTime);
+                    }
+                    yield return null;
+                }
+            }
+            var routine = StartCoroutine(onUpdate());
+            s.OnCleanup(() => StopCoroutine(routine));
+        };
+
+        EffectBlock IdleBehaviour => s => {
+            IEnumerator onUpdate() {
+                const float minInterval = 1f;
+                const float maxInterval = 3f;
+                while (true) {
+                    yield return new WaitForSeconds(Random.Range(minInterval, maxInterval));
+                    var angle = Random.Range(0f, Mathf.PI * 2f);
+                    targetDirection = new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle));
+                }
+            }
+            var routine = StartCoroutine(onUpdate());
+            s.OnCleanup(() => StopCoroutine(routine));
+        };
+
+        void OnDrawGizmosSelected() {
+            var circle = new Circle(transform.position, range);
+            circle.DrawGizmo(Color.red);
+        }
+    }
+}
