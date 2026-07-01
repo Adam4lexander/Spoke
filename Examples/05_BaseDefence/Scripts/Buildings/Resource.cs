a9526ecedfda4952aab0c324725c137716db6a33
Adam Alexander <16426184+Adam4lexander@users.noreply.github.com>
2026-07-01 21:00:09 +1000

    resource uses power node

--- Examples/05_BaseDefence/Scripts/Buildings/Resource.cs
+++ Examples/05_BaseDefence/Scripts/Buildings/Resource.cs
@@ -7,37 +7,19 @@ namespace Spoke.Examples.BaseDefence {
     public class Resource : SpokeBehaviour {
 
         [Header("References")]
-        [SerializeField] GameObject harvestEffect;
-
-        [Header("Attributes")]
-        [SerializeField] float radius;
+        [SerializeField] GameObject harvestFX;
+        [SerializeField] PowerNode powerNode;
 
         protected override void Init(EffectBuilder s) {
-            harvestEffect.SetActive(false);
+            harvestFX.SetActive(false);
             s.Phase(IsEnabled, s => {
-                s.Effect(Harvest);
+                s.Phase(powerNode.HasPower, Harvest);
             });
         }
 
         EffectBlock Harvest => s => {
-            var sensor = s.Use(GameState.PowerZone.AddSensor(() => new Circle(transform.position, radius)));
-
-            var isHarvested = s.Memo(s => {
-                foreach (var body in sensor.Overlaps) {
-                    if (body.Owner.IsProvider) return true;
-                }
-                return false;
-            }, sensor.OverlapsChanged);
-
-            s.Phase(isHarvested, s => {
-                harvestEffect.SetActive(true);
-                s.OnCleanup(() => harvestEffect.SetActive(false));
-            });
+            harvestFX.SetActive(true);
+            s.OnCleanup(() => harvestFX.SetActive(false));
         };
-
-        void OnDrawGizmosSelected() {
-            var circle = new Circle(transform.position, radius);
-            circle.DrawGizmo(Color.cyan);
-        }
     }
 }
