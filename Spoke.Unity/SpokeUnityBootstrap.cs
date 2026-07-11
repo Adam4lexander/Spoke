ace25e2012181bc5122352cf5d1dbb62a1e4ae29
Adam Alexander <16426184+Adam4lexander@users.noreply.github.com>
2026-07-11 10:46:53 +1000

    Dependants can ensure SpokeUnityBootstrap is initialized

--- Spoke.Unity/SpokeUnityBootstrap.cs
+++ Spoke.Unity/SpokeUnityBootstrap.cs
@@ -16,14 +16,14 @@ namespace Spoke {
 #if UNITY_EDITOR
     [InitializeOnLoad]
 #endif
-    internal static class SpokeUnityBootstrap {
+    public static class SpokeUnityBootstrap {
         static bool isInitialized;
 
-        static SpokeUnityBootstrap() 
-            => Init();
+        static SpokeUnityBootstrap()
+            => EnsureInitialized();
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static void Init() {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void EnsureInitialized() {
             if (isInitialized) return;
             isInitialized = true;
             SpokeError.Log = (msg, ex) => Debug.LogError($"[Spoke] {msg}\n{ex}");
